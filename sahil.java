a48c196c850558199d97da604b9833e93a00c75c
Abdullah <159717544+afgsahil@users.noreply.github.com>
2024-02-12 00:23:38 -0800

    Add files via upload

---
+++ sahil.java
@@ -0,0 +1,10 @@
+public class sahil {
+     
+    public void sum(){
+        int a=10;
+        int b=20;
+        int c= a+b;
+        System.out.println("the sum of this tow function is " +c);
+
+    }
+}
---
+++ test.java
@@ -0,0 +1,20 @@
+import java.util.Scanner;
+
+public class test {
+    public static void main(String[] args) {
+        Scanner scanner= new  Scanner(System.in);
+        // int num1;
+        // int num2;
+        // System.out.println("please enter tow numbers");
+        // num1= scanner.nextInt();
+        // num2= scanner.nextInt();
+        // int num3 = num1+num2;
+        // System.out.println("the sumation is "+num3);
+        sahil obj = new sahil();
+        obj.sum();
+        System.out.println("sahil khan i s");
+
+
+    }
+    
+}
